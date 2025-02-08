d0183f0d9b120387b7121596ba610cb99afd3149
NatanRodrigo <natanrodrigo4@gmail.com>
2025-02-08 10:12:48 -0300

    Inicio do projeto

---
+++ Entidades/Fluxo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExemploTemplateConsole.Entidades;
+
+namespace ExemploTemplateConsole.Entidades
+{
+    public class Fluxo
+    {
+        public void Executar()
+        {
+            // declarei uma váriavel do tipo pessoa
+            // coloquei o nome da váriavel como pessoa
+            // inicializei usando o igual
+            // recebendo o valor instanciando através do new
+            // que o new instaciou a classe Pessoa
+            // fiz com que a váriavel pessoa tenha todos os atributos da claase
+            Pessoa pessoa = new Pessoa();
+
+            // Quando eu coloco o nome da váriavel que eu instanciei um objeto (classe)
+            // através do .(ponto) eu consigo acessar e modificar os atribustos que tem na classe
+            // ou seja o nome, está dentro de class Pessoa 
+            // public string Nome { get; set; } pelo fato dele está public eu consigo acessar ele aqui
+            pessoa.Nome = "Natan";
+
+            pessoa.Nascimento = DateTime.Parse("2001-02-15");
+
+            TimeSpan tempoEntreDatas = DateTime.Now - pessoa.Nascimento;
+
+            DateTime quantosAnosTem = new DateTime(tempoEntreDatas.Ticks);
+
+            int quantosAnos = quantosAnosTem.Year;
+
+            if (quantosAnos < 18)
+            {
+                Console.WriteLine("Menor de idade");
+            }
+            else
+            {
+                Console.WriteLine("Maior de idade");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using ExemploTemplateConsole.Entidades;
+
+
+namespace ExemploTemplateConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Fluxo fluxo = new Fluxo();
+            fluxo.Executar();
+        }
+    }
+}
